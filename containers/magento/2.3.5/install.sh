808d13c766c820535c21bc4a729b852a66b7eab9
gabidavila <gabidavila@gmail.com>
2020-09-30 14:43:14 -0400

    Configuring Magento in case of reboot

--- containers/magento/2.3.5/install.sh
+++ containers/magento/2.3.5/install.sh
@@ -13,15 +13,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-if [[ $(/var/www/html/bin/magento setup:db:status --no-ansi) == "No information is available: the Magento application is not installed." ]] ;  then 
-  echo "Configuring MySQL"
+echo "Configuring MySQL Permissions"
   mysql -h $DB_HOST -u magento -p$DB_PASSWORD -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'magento'@'%';"
+
+TOTAL_TABLES_STORE=$(mysql -h $DB_HOST -u magento -D $DB_NAME  -p$DB_PASSWORD -e "SELECT count(*) AS TOTALNUMBEROFTABLES FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$DB_NAME'" -N)
+
+if [[ $TOTAL_TABLES_STORE == "0" ]] ;  then 
   echo "Installing Magento"
   php -dmemory_limit=5G /var/www/html/bin/magento setup:install --base-url=http://$(echo $WEBSITE_URL)/ --db-host="$DB_HOST" --db-name="$DB_NAME" --db-user=magento --db-password="$DB_PASSWORD" --enable-syslog-logging=1 --admin-firstname=admin --admin-lastname=admin --admin-email=admin@admin.com --admin-user=admin --admin-password="$ADMIN_PASSWORD" --language=en_US --currency=USD --timezone=America/New_York --use-rewrites=1 --use-sample-data
  else
-  echo 'yes'
+  echo "Configuring Magento"
+  php -dmemory_limit=5G /var/www/html/bin/magento setup:config:set -q -n --db-host="$DB_HOST" --db-name="$DB_NAME" --db-user=magento --db-password="$DB_PASSWORD" --enable-syslog-logging=1
 fi
 
 cd /var/www/html && chmod -R 777 var/ pub/ generated/
+bin/magento setup:upgrade
+bin/magento info:adminuri
 /usr/sbin/apache2ctl -D FOREGROUND
