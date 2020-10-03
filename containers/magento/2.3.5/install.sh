68138752cd87b408d9ebe7459bc05f2d756feb28
gabidavila <gabidavila@gmail.com>
2020-10-03 12:18:14 -0400

    Fixing Running Conditions

--- containers/magento/2.3.5/install.sh
+++ containers/magento/2.3.5/install.sh
@@ -18,13 +18,14 @@ mysql -h $DB_HOST -u magento -p$DB_PASSWORD -e "GRANT ALL PRIVILEGES ON $DB_NAME
 
 TOTAL_TABLES_STORE=$(mysql -h $DB_HOST -u magento -D $DB_NAME  -p$DB_PASSWORD -e "SELECT count(*) AS TOTALNUMBEROFTABLES FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$DB_NAME'" -N)
 
-if [[ $TOTAL_TABLES_STORE == "0" ]] ;  then 
+if [[ $TOTAL_TABLES_STORE == "0" || $TOTAL_TABLES_STORE == "" ]] ;  then 
   echo "Installing Magento"
   php -dmemory_limit=5G /var/www/html/bin/magento setup:install --base-url=http://$(echo $WEBSITE_URL)/ --db-host="$DB_HOST" --db-name="$DB_NAME" --db-user=magento --db-password="$DB_PASSWORD" --enable-syslog-logging=1 --admin-firstname=admin --admin-lastname=admin --admin-email=admin@admin.com --admin-user=admin --admin-password="$ADMIN_PASSWORD" --language=en_US --currency=USD --timezone=America/New_York --use-rewrites=1 --use-sample-data -vvv
  else
   echo "Configuring Magento"
   php -dmemory_limit=5G /var/www/html/bin/magento module:enable --all --clear-static-content -vvv
   php -dmemory_limit=5G /var/www/html/bin/magento setup:di:compile -vvv
+  cd /var/www/html && chmod -R 777 var/ pub/ generated/
   php -dmemory_limit=5G /var/www/html/bin/magento setup:config:set -n --db-host="$DB_HOST" --db-name="$DB_NAME" --db-user=magento --db-password="$DB_PASSWORD" --enable-syslog-logging=1 -vvv
 fi
 
